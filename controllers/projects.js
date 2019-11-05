c91780f69ed70270067b535a6a1c30ed1c4214af
Jewls Krueger <jewls@Jewlss-MacBook-Pro.local>
2019-11-05 10:13:13 -0800

    create new project route works

--- controllers/projects.js
+++ controllers/projects.js
@@ -3,14 +3,32 @@
 //----------------------
 const express = require('express');
 const router = express.Router();
-const Fruit = require('../models/project.js')
+const Project = require('../models/project.js')
 
 //----------------------
 // Routes
 //----------------------
+
+// new project page show route
+router.get('/new', (req, res) => {
+  res.render('projects/new.ejs')
+}) // end of new project show route
+
+// new project create route
+router.post('/', (req, res) => {
+  Project.create(req.body, (error, createdProject) => {
+    res.redirect('/projects')
+  })
+}) // end of create new project post route
+
+// projects home page route
 router.get('/', (req, res) => {
-  res.render('projects/home.ejs')
-})
+  Project.find({}, (error, allProjects) => {
+      res.render('projects/home.ejs', {
+        projects:allProjects
+      })
+  })
+}) // end of projects homepage route
 
 //----------------------
 // Export
